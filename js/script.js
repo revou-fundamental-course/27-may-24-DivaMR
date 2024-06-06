6f8d14074d1c1df9cfc8a8b9b6cce767b055cd6f
= <=>
2024-06-06 20:25:32 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 // ini js
+function replaceName () {
+    let name = prompt ("Halo, Siapa nama anda?", "");
+    document.getElementById("name"). innerHTML = name
+}
+
+replaceName();
+
+let namaInput = document.getElementById("nama")
+
+// form validation
+function formValidation() {
+    let namaInput = document.getElementById('nama').value;
+    console.log(namaInput);
+}
+
+// form-input
+
+
+let indexSlide = 1;
+showBanner(1);
+
+function nextSlide(n) {
+    showBanner(indexSlide += n);
+}
+
+function showBanner(indexBanner) {
+    let listImage = document.getElementsByClassName('banner-img');
+    if (indexBanner > listImage.length) indexSlide = 1;
+    
+    let index = 0;
+    while (index < listImage.length) {
+        listImage[index].style.display = 'none';
+        index++;
+    }
+
+    listImage[indexSlide - 1].style.display = 'block';
+    console.log('index ' + (indexSlide) + ' Adalah index image yang ditampilkan');
+}
+
+setInterval (() => nextSlide(1),  3000); 
